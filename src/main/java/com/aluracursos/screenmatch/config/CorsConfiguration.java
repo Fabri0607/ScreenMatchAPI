063822203e4d20fd1e9b351c09aa6b88a65f8e45
Fabricio Alfaro <119895626+Fabri0607@users.noreply.github.com>
2024-06-10 15:16:55 -0600

    Commit Final

---
+++ src/main/java/com/aluracursos/screenmatch/config/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package com.aluracursos.screenmatch.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
